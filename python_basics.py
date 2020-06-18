5b5c0e48f985af15e9afceb260604e78a20f27bb
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-06-18 15:41:06 +0700

    Add function pythagorean theorem

--- python_basics.py
+++ python_basics.py
@@ -1,3 +1,11 @@
+import math
+
 def hello(name):
     correctName = " ".join(name.split())
     print('Hello ' + correctName + '!')
+
+def calculate_hypotenuse(a, b):
+    c = math.sqrt(a**2 + b**2)
+    print(c)
+
+
