aab4162045de0dab9e94897a7d017a73f5f0dc6b
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-06-18 15:57:48 +0700

    Add function all conditions true

--- python_basics.py
+++ python_basics.py
@@ -8,3 +8,11 @@ def hello(name):
 def calculate_hypotenuse(a, b):
     c = math.sqrt(a**2 + b**2)
     return c
+
+def are_all_conditions_true(conditions):
+    if len(conditions) == 0:
+        return None
+    for x in conditions:
+        if x == False:
+            return False
+    return True
